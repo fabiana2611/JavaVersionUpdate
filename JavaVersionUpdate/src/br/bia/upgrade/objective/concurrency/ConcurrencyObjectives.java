dc2aab003020df537cb066b2b82a0f5bc7704305
fabiana <fabiana@192.168.25.54>
2018-03-28 20:23:50 -0300

    Add lock examples

--- JavaVersionUpdate/src/br/bia/upgrade/objective/concurrency/ConcurrencyObjectives.java
+++ JavaVersionUpdate/src/br/bia/upgrade/objective/concurrency/ConcurrencyObjectives.java
@@ -10,10 +10,7 @@ public class ConcurrencyObjectives {
 	public static void init() {
 		ConcurrencyExamples.init();
 		
-		//TODO
-//		Use Lock, ReadWriteLock, and ReentrantLock classes in the 
-//		java.util.concurrent.locks and java.util.concurrent.atomic packages to support 
-//		lock-free thread-safe programming on single variables
+		LockExamples.init();
 		
 		//TODO
 		//Thread Basics
